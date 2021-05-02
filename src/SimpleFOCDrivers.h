3365f02ce5ef180e6d6b9aff19343d5bb67c298d
Richard Unger <runger@aon.at>
2021-05-02 16:32:15 +0200

    enable arduino IDE to autodetect the library

---
+++ src/SimpleFOCDrivers.h
@@ -0,0 +1,11 @@
+
+#ifndef __SIMPLEFOC_DRIVERS_H__
+#define __SIMPLEFOC_DRIVERS_H__
+
+// empty header file. In Arduino IDE, include this header to enable the
+// IDE to "find" the library.
+// Then include the headers for the individual drivers/sensors/utilities
+// that you want to use.
+
+#endif
+
