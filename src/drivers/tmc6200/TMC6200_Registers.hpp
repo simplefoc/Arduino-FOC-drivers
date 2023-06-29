5c407eacdbd68429682e653a16aa80dfe36cab36
Yaseen M. Twati <twati.yaseen@outlook.com>
2023-06-29 09:36:29 +0200

    Reformat Code

--- src/drivers/tmc6200/TMC6200_Registers.hpp
+++ src/drivers/tmc6200/TMC6200_Registers.hpp
@@ -10,54 +10,54 @@
 
 // ------
 
-#define TMC6200_GCONF_REG          	0x00 // RW
-#define TMC6200_GSTAT_REG          	0x01 // RW + WC
-#define TMC6200_IOIN_REG   			0x04 // R
+#define TMC6200_GCONF_REG           0x00 // RW
+#define TMC6200_GSTAT_REG           0x01 // RW + WC
+#define TMC6200_IOIN_REG            0x04 // R
 
-#define TMC6200_OTP_PROG_REG       	0x06 // not used
-#define TMC6200_OTP_READ_REG       	0x07 // not used
-#define TMC6200_FACTORY_CONF_REG   	0x08 // not used
+#define TMC6200_OTP_PROG_REG        0x06 // not used
+#define TMC6200_OTP_READ_REG        0x07 // not used
+#define TMC6200_FACTORY_CONF_REG    0x08 // not used
 
-#define TMC6200_SHORT_CONF_REG     	0x09 // RW
-#define TMC6200_DRV_CONF_REG       	0x0A // RW
+#define TMC6200_SHORT_CONF_REG      0x09 // RW
+#define TMC6200_DRV_CONF_REG        0x0A // RW
 
 typedef union {
-	struct {
-		uint32_t DISABLE:1;
-		uint32_t SINGLE_LINE:1;
-		uint32_t FAULT_DIRECT:1;
-		uint32_t :1;
-		uint32_t AMPLIFICATION:2;
-		uint32_t AMPLIFIER_OFF:1;
-		uint32_t TEST_MODE:1;
-		uint32_t :24;
-	};
-	uint32_t reg;
+    struct {
+        uint32_t DISABLE: 1;
+        uint32_t SINGLE_LINE: 1;
+        uint32_t FAULT_DIRECT: 1;
+        uint32_t : 1;
+        uint32_t AMPLIFICATION: 2;
+        uint32_t AMPLIFIER_OFF: 1;
+        uint32_t TEST_MODE: 1;
+        uint32_t : 24;
+    };
+    uint32_t reg;
 } TMC6200_GCONF;
 
 typedef union {
-	struct {
-		uint32_t RESET:1;
-		uint32_t DRV_OTPW:1;
-		uint32_t DRV_OT:1;
-		uint32_t UV_CP:1;
+    struct {
+        uint32_t RESET: 1;
+        uint32_t DRV_OTPW: 1;
+        uint32_t DRV_OT: 1;
+        uint32_t UV_CP: 1;
 
-		uint32_t SHORT_DET_U:1;
-		uint32_t S2GU:1;
-		uint32_t S2VSU:1;
-		uint32_t :1;
+        uint32_t SHORT_DET_U: 1;
+        uint32_t S2GU: 1;
+        uint32_t S2VSU: 1;
+        uint32_t : 1;
 
-		uint32_t SHORT_DET_V:1;
-		uint32_t S2GV:1;
-		uint32_t S2VSV:1;
-		uint32_t :1;
+        uint32_t SHORT_DET_V: 1;
+        uint32_t S2GV: 1;
+        uint32_t S2VSV: 1;
+        uint32_t : 1;
 
-		uint32_t SHORT_DET_W:1;
-		uint32_t S2GW:1;
-		uint32_t S2VSW:1;
-		uint32_t :1;
-	};
-	uint32_t reg;
+        uint32_t SHORT_DET_W: 1;
+        uint32_t S2GW: 1;
+        uint32_t S2VSW: 1;
+        uint32_t : 1;
+    };
+    uint32_t reg;
 } TMC6200_GSTAT;
 
 typedef union {
@@ -82,32 +82,32 @@ typedef union {
 } TMC6200_IOIN;
 
 typedef union {
-	struct {
-		uint32_t S2VS_LEVEL:4;
-		uint32_t :4;
-		uint32_t S2G_LEVEL:4;
-		uint32_t :4;
-		uint32_t SHORT_FILTER:2;
-		uint32_t :2;
-		uint32_t SHORT_DELAY:1;
-		uint32_t :4;
-		uint32_t RETRY:2;
-		uint32_t :2;
-		uint32_t PROTECT_PARALLEL:1;
-		uint32_t DISABLE_S2G:1;
-		uint32_t DISABLE_S2VS:1;
-		uint32_t :2;
-	};
-	uint32_t reg;
+    struct {
+        uint32_t S2VS_LEVEL: 4;
+        uint32_t : 4;
+        uint32_t S2G_LEVEL: 4;
+        uint32_t : 4;
+        uint32_t SHORT_FILTER: 2;
+        uint32_t : 2;
+        uint32_t SHORT_DELAY: 1;
+        uint32_t : 4;
+        uint32_t RETRY: 2;
+        uint32_t : 2;
+        uint32_t PROTECT_PARALLEL: 1;
+        uint32_t DISABLE_S2G: 1;
+        uint32_t DISABLE_S2VS: 1;
+        uint32_t : 2;
+    };
+    uint32_t reg;
 } TMC6200_SHORT_CONF;
 
 typedef union {
-	struct {
-		uint32_t 	BBMCLKS:5;
-		uint32_t	:11;
-		uint32_t 	OT_SELECT:2;
-		uint32_t 	DRV_STRENGTH:2;
-		uint32_t	:12;
-	};
-	uint32_t reg;
+    struct {
+        uint32_t BBMCLKS: 5;
+        uint32_t    : 11;
+        uint32_t OT_SELECT: 2;
+        uint32_t DRV_STRENGTH: 2;
+        uint32_t    : 12;
+    };
+    uint32_t reg;
 } TMC6200_DRV_CONF;
