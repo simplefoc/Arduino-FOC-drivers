271ccae4473c5580dc9df4abddcc018deff7d067
gospar <antun.skuric@outlook.com>
2026-09-04 11:02:20 +0200

    fix for undefined referecne bug

--- src/comms/CommanderMaster.h
+++ src/comms/CommanderMaster.h
@@ -5,8 +5,8 @@
 
 class CommanderMaster {
 public:
-    CommanderMaster();
-    ~CommanderMaster();
+    CommanderMaster() = default;
+    virtual ~CommanderMaster() = default;
 
     virtual int writeRegister(int motor, SimpleFOCRegister registerNum, void* data, uint8_t size) = 0;
     virtual int readRegister(int motor, SimpleFOCRegister registerNum, void* data, uint8_t size) = 0;
